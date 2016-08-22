e2db0865d2817fc4e34014a689145f21afd388ff
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-08-22 15:41:13 +0200

    mathbot haxxx

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -2,7 +2,11 @@ module SlackMathbot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
       command 'calculate' do |client, data, _match|
-        client.say(channel: data.channel, text: '4')
+        words = data.text.split(" ")
+
+        words.each do |word|
+          client.say(channel: data.channel, text: word)
+        end
       end
     end
   end
