d6836e81a34fdc4a01431d9033d0b9cf65590f87
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-08-22 16:36:59 +0200

    selective echo

--- slack-mathbot/bot.rb
+++ slack-mathbot/bot.rb
@@ -1,8 +1,18 @@
 module SlackMathbot
   class Bot < SlackRubyBot::Bot
     operator '' do |client, data, match|
-      response = match.shift
-      client.say(channel: data.channel, text: response)
+      words = data.text.split(" ")
+      checkvar = false
+
+      words.each do |word|
+        if word == "oink"
+          checkvar = true
+        end
+      end
+
+      if checkvar
+        client.say(channel: data.channel, text: "Hey piggy, piggy, pig, pig!")
+      end
     end
   end
 end
