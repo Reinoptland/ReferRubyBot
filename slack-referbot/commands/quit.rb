dd80048377c717b7f6601d059eaed84b4b799f5c
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-08-24 11:46:24 +0200

    rename to abort to match branch name

--- slack-referbot/commands/quit.rb
+++ slack-referbot/commands/quit.rb
@@ -1,7 +1,7 @@
 module SlackReferbot
   module Commands
     class Referral < SlackRubyBot::Commands::Base
-      command 'quit' do |client, data, match|
+      command 'abort' do |client, data, match|
         client.say(text: referral.inspect, channel: data.channel)
       end
     end
