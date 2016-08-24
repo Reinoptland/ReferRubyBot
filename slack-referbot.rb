55c24e7b777040578fe8e1b11c262dd176dccd73
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-08-24 11:02:44 +0200

    remove some testing stuff that became obsolete with Redis implementation

--- slack-referbot.rb
+++ slack-referbot.rb
@@ -4,5 +4,3 @@ require 'slack-referbot/commands/calculate'
 require 'slack-referbot/commands/vacancylist'
 require 'slack-referbot/commands/testconversation'
 require 'slack-referbot/bot'
-
-@@in_conversation = false
