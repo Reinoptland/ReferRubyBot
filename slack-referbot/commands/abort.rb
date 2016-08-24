318b3a0318be96fdc0884938e056b856a313657b
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-08-24 12:58:45 +0200

    abort process function basic functionality

--- slack-referbot/commands/abort.rb
+++ slack-referbot/commands/abort.rb
@@ -2,10 +2,14 @@ module SlackReferbot
   module Commands
     class Referral < SlackRubyBot::Commands::Base
       command 'abort' do |client, data, match|
+        identifier = match['expression']
         redis = Redis.current
-        referral = redis.hgetall(identifier)
 
-        client.say(text: "#{redis.inspect} #{match.inspect} #{referral}", channel: data.channel)
+        client.say(text: "#{match.inspect}", channel: data.channel)
+
+        redis.del(identifier)
+
+        client.say(text: "whois #{identifier}", channel: data.channel)
       end
     end
   end
