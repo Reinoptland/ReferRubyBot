c50b87b181fc5cecedbc15390afb044892ecf6ac
reinoptland <reinoptland@gmail.com>
2016-08-26 11:50:57 +0200

    list command only prints out the latest vacancy

--- slack-referbot/commands/vacancylist.rb
+++ slack-referbot/commands/vacancylist.rb
@@ -10,9 +10,8 @@ module SlackReferbot
         test1 = JSON.parse(offers, symbolize_names: true)
           contents = test1[:offers]
           contents = contents.sort_by { |vacancy| vacancy[:created_at] }
-          contents.each_with_index do |content, index|
-            client.say(channel: data.channel, text: "#{index + 1}, #{content[:title]} \n #{content[:careers_url]} \n #{content[:created_at]}\n")
-          end
+          content = contents.first
+          client.say(channel: data.channel, text: " #{content[:title]} \n #{content[:careers_url]}")
 
         client.say(channel: data.channel, text: "")
       end
