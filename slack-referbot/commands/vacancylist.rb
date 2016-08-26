87d933b0843512f4984ed397b47c7e3dc0336c53
reinoptland <reinoptland@gmail.com>
2016-08-26 11:07:12 +0200

    Sort vacancylist by title

--- slack-referbot/commands/vacancylist.rb
+++ slack-referbot/commands/vacancylist.rb
@@ -3,12 +3,13 @@ module SlackReferbot
     class Vacancylist < SlackRubyBot::Commands::Base
       command 'list' do |client, data, _match|
 
-        $workaround =""
+        
 
         offers = HTTP.get('https://api.recruitee.com/c/referbot/careers/offers')
 
         test1 = JSON.parse(offers, symbolize_names: true)
           contents = test1[:offers]
+          contents = contents.sort_by { |vacancy| vacancy[:title] }
           contents.each_with_index do |content, index|
             client.say(channel: data.channel, text: "#{index + 1}, #{content[:title]} \n #{content[:careers_url]} \n #{content[:created_at]}\n")
           end
