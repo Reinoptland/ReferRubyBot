ef87bd1ca1a5174b4bfeecb7a51526da6d864b37
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-08-26 15:02:38 +0200

    progression guard

--- slack-referbot/bot.rb
+++ slack-referbot/bot.rb
@@ -1,38 +1,66 @@
 module SlackReferbot
   class Bot < SlackRubyBot::Bot
     conversation_states = {
-      in_conversation: false,
-      state_name: false
+      state_in_conversation: false,
+      state_get_name: false
+    }
+
+    referee = {
+      name: '',
+      email: ''
     }
 
     operator '' do |client, data, match|
 
-      if /(^refer| refer)/i.match(data.text) && conversation_states[:in_conversation] == false
-        # Get user's DM channel code
-        uri = URI("https://slack.com/api/im.open?token=#{ENV['SLACK_API_TOKEN']}&user=#{data.user}")
-        # Needs reformatting with names to symbol
-        uri_response = JSON.parse(Net::HTTP.get(uri))
-        @dm_channel = uri_response["channel"]["id"]
+      client.say(channel: data.channel, text: data.text)
 
-        client.say(channel: @dm_channel, text: "Hi <@#{data.user}>. Would you like to refer someone to one of our open vacancies?")
+      # User types refer
+      if /(^refer| refer)/i.match(data.text) && conversation_states[:state_in_conversation] == false
+        # # Get user's DM channel code
+        # uri = URI("https://slack.com/api/im.open?token=#{ENV['SLACK_API_TOKEN']}&user=#{data.user}")
+        # # Needs reformatting with names to symbol
+        # uri_response = JSON.parse(Net::HTTP.get(uri))
+        # dm_channel = uri_response["channel"]["id"]
 
-        conversation_states[:in_conversation] = true
+        client.say(channel: data.channel, text: "Hi <@#{data.user}>. Would you like to refer someone to one of our open vacancies?")
+
+        conversation_states[:state_in_conversation] = true
+
       end
 
-      if /^y/i.match(data.text) && conversation_states[:in_conversation]
-        client.say(channel: data.channel, text: "Excellent, let's get started!#{@dm_channel}")
-        sleep(0.5)
-        client.say(channel: @dm_channel, text: "What is your referee's name?")
+      # User is asked to confirm to start referring process
+      if /^y/i.match(data.text) && conversation_states[:state_in_conversation]
 
-      elsif /^n/i.match(data.text) && conversation_states[:in_conversation]
-        client.say(channel: @dm_channel, text: "My bad!")
+        client.say(channel: data.channel, text: "Excellent, let's get started!")
         sleep(0.5)
-        client.say(channel: @dm_channel, text: "Feel free to contact me any time you want to refer someone to our company!")
-        conversation_states[:in_conversation] = true
+        client.say(channel: data.channel, text: "What is your referee's name?")
 
-      elsif conversation_states[:in_conversation]
-        client.say(channel: @dm_channel, text: "I didn't quite get that... Do you want to refer someone?")
+        conversation_states[:state_get_name] = true
+        progression_guard = data.text
+
+      elsif /^n/i.match(data.text) && conversation_states[:state_in_conversation]
+        client.say(channel: data.channel, text: "My bad!")
+        sleep(0.5)
+        client.say(channel: data.channel, text: "Feel free to contact me any time you want to refer someone to our company!")
+
+        conversation_states[:state_in_conversation] = false
+
+      # elsif !/(^y|^n)/i.match(data.text) && conversation_states[:state_in_conversation]
+      #   client.say(channel: data.channel, text: "I didn't quite get that... Do you want to refer someone?")
+      #
+      #   client.instance_variable_get(:@callbacks)['message'].pop
+
       end
+
+      # User is asked to provide a name
+      if progression_guard != data.text && conversation_states[:state_get_name]
+        referee[:name] << data.text
+        client.say(channel: data.channel, text: "#{data.text}, what a lovely name!")
+
+        client.say(channel: data.channel, text: "We will have to contact #{referee[:name]} as well... Do you have an e-mail address?")
+
+      end
+
 
     end # Outer operator
   end # Class
