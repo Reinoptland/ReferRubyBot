73f49d804fd07db645ccc57a46b6f4b77c50bee4
reinoptland <reinoptland@gmail.com>
2016-08-26 10:25:17 +0200

    Add created_at to list command

--- slack-referbot/commands/vacancylist.rb
+++ slack-referbot/commands/vacancylist.rb
@@ -10,7 +10,7 @@ module SlackReferbot
         test1 = JSON.parse(offers, symbolize_names: true)
           contents = test1[:offers]
           contents.each_with_index do |content, index|
-            client.say(channel: data.channel, text: "#{index + 1}, #{content[:title]} \n #{content[:careers_url]} \n")
+            client.say(channel: data.channel, text: "#{index + 1}, #{content[:title]} \n #{content[:careers_url]} \n #{content[:created_at]}\n")
           end
 
         client.say(channel: data.channel, text: "")
