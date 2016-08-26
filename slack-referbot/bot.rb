ee51e8e06a7892662314f44915e90c46d6069794
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-08-26 15:22:47 +0200

    reformat basic functionality

--- slack-referbot/bot.rb
+++ slack-referbot/bot.rb
@@ -2,7 +2,10 @@ module SlackReferbot
   class Bot < SlackRubyBot::Bot
     conversation_states = {
       state_in_conversation: false,
-      state_get_name: false
+      state_get_name: false,
+      state_get_email: false,
+      state_get_vacancy: false,
+      state_input_gathered: false
     }
 
     referee = {
@@ -53,16 +56,70 @@ module SlackReferbot
 
       end
 
-      # User is asked to provide a name
+      # User is asked to provide a name...
       if progression_guard != data.text && conversation_states[:state_get_name]
         referee[:name] << data.text
-        client.say(channel: data.channel, text: "#{data.text}, what a lovely name!")
+        client.say(channel: data.channel, text: "#{referee[:name].capitalize}, what a lovely name!")
 
         client.say(channel: data.channel, text: "We will have to contact #{referee[:name]} as well... Do you have an e-mail address?")
 
+        conversation_states[:state_get_email] = true
+        progression_guard = data.text
+
       end
 
+      # ... email...
+      if progression_guard != data.text && conversation_states[:state_get_email]
+        referee[:email] << data.text
+
+        client.say(channel: data.channel, text: "Great! Let me show you a list of vacancies that we are looking to fill.")
+
+        list = getlist
+
+        client.say(channel: data.channel, text: "Please enter the vacancy that you would like to recommend #{referee[:name]} for. If you think an open application would be more appropriate, just type 0!")
+
+        conversation_states[:state_get_vacancy] = true
+        progression_guard = data.text
+      end
+
+      # ... vacancy...
+      if progression_guard != data.text && conversation_states[:state_get_vacancy]
+        referee[:vacancy] << data.text
+
+        conversation_states[:state_input_gathered] = true
+        progression_guard = data.text
+      end
+
+      if conversation_states[:state_input_gathered]
+        client.say(channel: data.channel, text: "Awesome! You have finished referring #{referee[:name]} for the job of #{referee[:vacancy]}. We'll initiate contact at #{referee[:email]} as soon as possible!")
+
+        broadcast_to_general
+      end
 
     end # Outer operator
   end # Class
 end # Module
+
+def getlist
+  receivelist = HTTP.get('https://api.recruitee.com/c/referbot/careers/offers')
+  test1 = JSON.parse(receivelist, symbolize_names: true)
+  contents = test1[:offers]
+  list = ""
+  contents.each_with_index do |content, index|
+    list = list + "#{index+1}, #{content[:title]} \n #{content[:careers_url]} \n"
+  end
+  return list
+end
+
+def broadcast_to_general
+  uri = URI("https://slack.com/api/channels.list?token=#{ENV['SLACK_API_TOKEN']}")
+
+  # Needs reformatting with names to symbol
+  uri_response = JSON.parse(Net::HTTP.get(uri))
+
+  # Assuming #general is always the first item in the channels
+  # array. Needs non-hard coded fix
+  general_channel = uri_response["channels"][0]["id"]
+
+  client.say(text: "Someone has just referred a friend to come join our company! What are your excuses, meatbags?", channel: general_channel)
+end
