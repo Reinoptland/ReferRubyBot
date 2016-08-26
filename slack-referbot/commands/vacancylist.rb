19a74a5bfacdd153c91b8139a05d8897722aecee
reinoptland <reinoptland@gmail.com>
2016-08-26 11:17:38 +0200

    Order vacancylist by created_at

--- slack-referbot/commands/vacancylist.rb
+++ slack-referbot/commands/vacancylist.rb
@@ -3,13 +3,13 @@ module SlackReferbot
     class Vacancylist < SlackRubyBot::Commands::Base
       command 'list' do |client, data, _match|
 
-        
+
 
         offers = HTTP.get('https://api.recruitee.com/c/referbot/careers/offers')
 
         test1 = JSON.parse(offers, symbolize_names: true)
           contents = test1[:offers]
-          contents = contents.sort_by { |vacancy| vacancy[:title] }
+          contents = contents.sort_by { |vacancy| vacancy[:created_at] }
           contents.each_with_index do |content, index|
             client.say(channel: data.channel, text: "#{index + 1}, #{content[:title]} \n #{content[:careers_url]} \n #{content[:created_at]}\n")
           end
