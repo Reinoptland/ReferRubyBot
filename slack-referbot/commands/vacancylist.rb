f0e51350c5c9463e21c2670b0e3e07d8792e96ad
reinoptland <reinoptland@gmail.com>
2016-08-26 14:11:52 +0200

    Add script to check if latest vacancy was created today

--- slack-referbot/commands/vacancylist.rb
+++ slack-referbot/commands/vacancylist.rb
@@ -3,17 +3,25 @@ module SlackReferbot
     class Vacancylist < SlackRubyBot::Commands::Base
       command 'list' do |client, data, _match|
 
+      offers = HTTP.get('https://api.recruitee.com/c/referbot/careers/offers')
+
+      test1 = JSON.parse(offers, symbolize_names: true)
+      contents = test1[:offers]
+      contents = contents.sort_by { |vacancy| vacancy[:created_at] }
+      content = contents.first
+      latest_vacancy_date = Date.parse content[:created_at]
+      latest_vacancy_date = latest_vacancy_date.to_s
+      today = Time.now.strftime("%Y-%d-%m")
 
 
-        offers = HTTP.get('https://api.recruitee.com/c/referbot/careers/offers')
 
-        test1 = JSON.parse(offers, symbolize_names: true)
-          contents = test1[:offers]
-          contents = contents.sort_by { |vacancy| vacancy[:created_at] }
-          content = contents.first
-          client.say(channel: data.channel, text: " #{content[:title]} \n #{content[:careers_url]}")
+      client.say(channel: data.channel, text: " #{content[:title]} \n #{content[:careers_url]} \n #{Time.now.strftime("at %I:%M%p")} \n #{today} \n #{} ")
 
-        client.say(channel: data.channel, text: "")
+        if today === latest_vacancy_date
+        client.say(channel: data.channel, text: "oink")
+        else
+        client.say(channel: data.channel, text: "boo!")
+        end
       end
     end
   end
